4856405f2c33629548a215f0a8dc2e9a94364fb8
Vlake <apulivic@gmail.com>
2022-11-22 21:01:49 +0800

    adding custom JS

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,8 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+// import "/app/javascript/custom/comic.js"
+// require("comic")
+require()
+
+
---
+++ app/javascript/custom/comic.js
@@ -0,0 +1,7 @@
+loader = document.querySelector(".loader-container")
+
+const timer = () => {
+  loader.display = "none"
+}
+
+setTimeout(timer, 2000)
